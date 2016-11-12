c41446a6240c774cb06105a4cdb7272c723a5c29
maxim1999 <maxbax.1999@mail.ru>
2016-11-12 20:58:57 +0300

    Update num_to_alpha.cpp

--- num_to_alpha.cpp
+++ num_to_alpha.cpp
@@ -13,34 +13,6 @@ int main(int argc, char** argv) {
     char c;
     cout << "Enter a number from 1-26: ";
     cin >> ch;
-    switch (ch) {
-        case 1: c = 'a'; break;
-        case 2: c = 'b'; break;
-        case 3: c = 'c'; break;
-        case 4: c = 'd'; break;
-        case 5: c = 'e'; break;
-        case 6: c = 'f'; break;
-        case 7: c = 'g'; break;
-        case 8: c = 'h'; break;
-        case 9: c = 'i'; break;
-        case 10: c = 'j'; break;
-        case 11: c = 'k'; break;
-        case 12: c = 'l'; break;
-        case 13: c = 'm'; break;
-        case 14: c = 'n'; break;
-        case 15: c = 'o'; break;
-        case 16: c = 'p'; break;
-        case 17: c = 'q'; break;
-        case 18: c = 'r'; break;
-        case 19: c = 's'; break;
-        case 20: c = 't'; break;
-        case 21: c = 'u'; break;
-        case 22: c = 'v'; break;
-        case 23: c = 'w'; break;
-        case 24: c = 'x'; break;
-        case 25: c = 'y'; break;
-        case 26: c = 'z'; break;
-    }
-    cout << c;
+    cout << "abcdefghijklmnopqrstuvwxyz"[ch - 1];
     return 0;
 }
